07d502ae483595937c0fd580537cf982557462fb
Ethan Torres <torresethan99@gmail.com>
2020-05-31 15:09:51 -0500

    Project Homepage version 0.1.0 alpha

---
+++ example.js
@@ -0,0 +1,25 @@
+var number= 0;
+for (number=0;number++;number>5);
+document.write(number + "<br>");
+
+switch(number)
+{
+    case 1:
+        write("this is the number one.");
+        break;
+    case 2:
+        write("this is the number two.");
+        break;
+    case 3:
+        write("this is the number three.");
+        break;
+    case 4:
+        write("this is the number four.");
+        break;
+    case 5:
+        write("this is the number we are looking for.");
+        break;
+    default:
+        write("we couldn't find any numbers");
+        break;
+}
